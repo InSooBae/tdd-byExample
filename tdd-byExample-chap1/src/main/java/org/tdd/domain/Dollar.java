a5297d6af2ba7ee0f05cb8b4338c9324bc1aab5c
qospwmf <qospwmf@gmail.com>
2023-04-22 19:21:15 +0900

    [chap1] 5*2 상수를 생성자와 메서드로 중복 제거

--- tdd-byExample-chap1/src/main/java/org/tdd/domain/Dollar.java
+++ tdd-byExample-chap1/src/main/java/org/tdd/domain/Dollar.java
@@ -4,9 +4,10 @@ public class Dollar {
     int amount;
 
     public Dollar(int amount) {
-//        this.amount = amount;
+        this.amount = amount;
     }
 
     public void times(int multiplier) {
+        this.amount *= multiplier;
     }
 }

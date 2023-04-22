f5293277904bfaffff1e7042c830288a51be5739
qospwmf <qospwmf@gmail.com>
2023-04-22 23:37:14 +0900

    [chap2] product한 뒤 결과값이 연이어 적용되는 부작용 제거

--- tdd-byExample-chap1/src/main/java/org/tdd/domain/Dollar.java
+++ tdd-byExample-chap1/src/main/java/org/tdd/domain/Dollar.java
@@ -8,8 +8,6 @@ public class Dollar {
     }
 
     public Dollar times(int multiplier) {
-        this.amount *= multiplier;
-
-        return null;
+        return new Dollar(this.amount * multiplier);
     }
 }

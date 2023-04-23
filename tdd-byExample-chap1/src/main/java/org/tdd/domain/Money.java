72caf96b97af4e04ceac5c0a869364dca1556a3d
qospwmf <qospwmf@gmail.com>
2023-04-24 04:12:01 +0900

    [chap8] Money의 하위 클래스들이 큰 일을 하지 않는 것 같아 하위 클래스의 직접적인 참조가 적어지는 자체가 하위 클래스를 제거하기 위해 한 발짝 다가서는 것과 같다.(자식 클래스 정정 팩토리 메서드로 Money에서 생성, 공통점을 갖는 추상 클래스와 우선 times 메서드 추상화로 중복 제거를 향해 한 단계 전진)

--- tdd-byExample-chap1/src/main/java/org/tdd/domain/Money.java
+++ tdd-byExample-chap1/src/main/java/org/tdd/domain/Money.java
@@ -2,9 +2,18 @@ package org.tdd.domain;
 
 import java.util.Objects;
 
-public class Money {
+public abstract class Money {
     protected int amount;
 
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object o) {
--- tdd-byExample-chap1/src/test/java/org/tdd/domain/OperationTest.java
+++ tdd-byExample-chap1/src/test/java/org/tdd/domain/OperationTest.java
@@ -11,26 +11,26 @@ public class OperationTest {
     @Test
     @DisplayName("어떤 금액(주가)을 어떤 수(주식의 수)에 곱한 금액을 결과로 얻을 수 있어야 한다.")
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     @DisplayName("돈(금액)이 같은 돈(금액)인지(동치성)")
     public void testEquality() {
-        assertEquals(new Dollar(5), new Dollar(5));
-        assertNotEquals(new Dollar(5), new Dollar(6));
-        assertEquals(new Franc(5), new Franc(5));
-        assertNotEquals(new Franc(5), new Franc(6));
-        assertNotEquals(new Dollar(5), new Franc(5));
+        assertEquals(Money.dollar(5), Money.dollar(5));
+        assertNotEquals(Money.dollar(5), Money.dollar(6));
+        assertEquals(Money.franc(5), Money.franc(5));
+        assertNotEquals(Money.franc(5), Money.franc(6));
+        assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
     @DisplayName("프랑을 곱한 금액을 결과로 얻을 수 있어야 한다.")
     public void testFrancMultiplication() {
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 }
